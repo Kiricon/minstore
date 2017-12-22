2117f51beb3beacf9273edcdb59f5d1b80247116
Dominic Valenciana <kiricon@live.com>
2017-12-21 21:00:48 -0700

    Example showing the spread working as expected

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,16 @@
 import minstore from '../main';
 
 const store = minstore({
-    count: 0
+    count: 0,
+    count2: 0
 });
 
 const counter = document.querySelector('#counter');
+const counter2 = document.querySelector('#counter2');
 
 store.subscribe((state) => {
     counter.innerHTML = state.count;
+    counter2.innerHTML = state.count2;
 });
 
 function increment() {
@@ -19,5 +22,15 @@ function increment() {
     }, 1000);
 }
 
+function increment2() {
+    setTimeout(() => {
+        store.setState({
+            count2: store.getState().count2 + 3
+        });
+        increment2();
+    }, 3000);
+}
+
 increment();
+increment2();
 
